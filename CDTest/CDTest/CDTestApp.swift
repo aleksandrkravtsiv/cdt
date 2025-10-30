bfa398c6984fabc35b8a67d50f5fe708f484a798
Oleksandr Kravtsiv <alexandrkravtsiv@gmail.com>
2025-10-30 13:55:36 +0200

    - created project files

---
+++ CDTest/CDTest/CDTestApp.swift
@@ -0,0 +1,21 @@
+//
+//  CDTestApp.swift
+//  CDTest
+//
+//  Created by Aleksandr Kravtsiv on 30.10.2025.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct CDTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
